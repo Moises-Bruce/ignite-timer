fd3684a6b658ecb40349371a1490f9b0b8b81243
Moises-Bruce <contato.moisesbrucearaujo@gmail.com>
2023-03-25 22:16:41 -0400

    apply styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Button } from "./components/Button"
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="success"/>
+      <Button variant="danger"/>
+      <Button />
+    </>
   )
 }
 
